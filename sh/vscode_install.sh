1752b72a539c22d889579e9962c6884c2058a3be
cti1650 <cti1650@gmail.com>
2021-04-14 20:01:34 +0900

    Update vscode_install.sh

--- sh/vscode_install.sh
+++ sh/vscode_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt install -y apt-transport-https \
-      echo -e "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list \
-      wget -O - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - \
-      sudo apt update \
-      sudo apt install -y code
+sudo apt install -y apt-transport-https
+echo -e "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+wget -O - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo apt update
+sudo apt install -y code
