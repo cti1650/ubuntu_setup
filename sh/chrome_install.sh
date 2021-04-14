ebbc6954c998bf2d12e74dc111d0afe9eb8c71df
cti1650 <cti1650@gmail.com>
2021-04-14 12:41:28 +0900

    Create chrome_install.sh

---
+++ sh/chrome_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+apt list --installed google*
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+ls -l /etc/apt/sources.list.d
+cat /etc/apt/sources.list.d/google.list
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt update
+apt list google*
+sudo apt-get install google-chrome-stable
+apt list --installed google*
